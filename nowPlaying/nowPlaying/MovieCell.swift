662a0389eb4670c8f87fc8bfca1fa017f4cd1eec
Terry Wen <wen.terry.e@gmail.com>
2016-01-07 20:42:42 -0700

    base project completion

---
+++ nowPlaying/nowPlaying/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  nowPlaying
+//
+//  Created by twen6 on 1/7/16.
+//  Copyright © 2016 Terry Wen. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
